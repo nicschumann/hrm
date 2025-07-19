37dd8566c5b23ca57558abbeb88d9c48367e54fc
nicschumann <nic@nicschumann.co>
2025-07-18 17:20:27 -0700

    [feat] adds really simple sequential tokenizer for mazes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from data.maze import MazeDataset
 from data.utils import save_image
+from model.tokenizers.maze import MazeTokenizer
 
 if __name__ == "__main__":
     d = MazeDataset("./cache")
+    tokenizer = MazeTokenizer()
     x, y = d[0]
 
-    print(x.shape, x.dtype)
-    print(y.shape, y.dtype)
+    x.unsqueeze_(0)  # NOTE(Nic): add batch dim for testing.
+    y.unsqueeze_(0)  # NOTE(Nic): add batch dim for testing.
 
-    save_image("test3.png", x)
+    x_seq, y_seq = tokenizer(x, y)
---
+++ model/tokenizers/maze.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from enum import IntEnum
+
+
+class InputTokens(IntEnum):
+    PAD = 0
+    WALL = 1
+    PATH = 2
+    SOURCE_POS = 3
+    TARGET_POS = 4
+    NEW_LINE = 5
+
+
+class OutputTokens(IntEnum):
+    PAD = 0
+    IGNORE = 2
+    ROUTE = 3
+    NEW_LINE = 4
+
+
+class MazeTokenizer(nn.Module):
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor | None = None):
+        assert x.dim() == 4, f"expecting B, C, W, H, got a tensor with dim={x.dim()}"
+        B, C, W, H = x.shape
+        assert C == 3, f"expecting 3-channel maze images, but got ({B}, {C}, {W}, {H})"
+
+        # input_seqs = torch.full((B, W, H + 1), fill_value=InputTokens.PAD)
+        input_grid = torch.full((B, W, H + 1), fill_value=InputTokens.NEW_LINE)
+        wall_mask = torch.where(
+            (x[:, 0, :, :] == 0) & (x[:, 1, :, :] == 0) & (x[:, 2, :, :] == 0)
+        )
+        path_mask = torch.where(
+            (x[:, 0, :, :] == 1) & (x[:, 1, :, :] == 1) & (x[:, 2, :, :] == 1)
+        )
+        start_mask = torch.where(
+            (x[:, 0, :, :] == 1) & (x[:, 1, :, :] == 0) & (x[:, 2, :, :] == 0)
+        )
+        end_mask = torch.where(
+            (x[:, 0, :, :] == 0) & (x[:, 1, :, :] == 1) & (x[:, 2, :, :] == 0)
+        )
+
+        input_grid[wall_mask] = InputTokens.WALL
+        input_grid[path_mask] = InputTokens.PATH
+        input_grid[start_mask] = InputTokens.SOURCE_POS
+        input_grid[end_mask] = InputTokens.TARGET_POS
+
+        if y is not None:
+            assert y.dim() == 3
+            B_out, W_out, H_out = y.shape
+            assert B_out == B and W_out == W and H_out == H
+
+            output_grid = torch.full_like(input_grid, fill_value=OutputTokens.NEW_LINE)
+            ignore_mask = torch.where(y == 0)
+            route_mask = torch.where(y == 1)
+            output_grid[ignore_mask] = OutputTokens.IGNORE
+            output_grid[route_mask] = OutputTokens.ROUTE
+
+            print(output_grid)
+        else:
+            output_grid = None
+
+        return input_grid.reshape(B, -1), output_grid

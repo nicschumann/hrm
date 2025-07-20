f483e59d00ff6ffbc45c0f74942f6856631bfe58
nicschumann <nic@nicschumann.co>
2025-07-20 09:27:53 -0700

    [feat] implements a first pass at the recurrent module, shapes work.

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from data.maze import MazeDataset
 from data.utils import save_image
 from model.tokenizers.maze import MazeTokenizer, InputTokens
 from model.input import InputNetwork
+from model.recurrence import RecurrentModule, get_dummy_vars
 
 if __name__ == "__main__":
     d_train = MazeDataset("./cache", train=True)
     d_test = MazeDataset("./cache", train=False)
+    d_model = 64
+    max_seq = 256
 
     tokenizer = MazeTokenizer()
-    f_input = InputNetwork(len(InputTokens), 256, 256)
+
+    f_input = InputNetwork(len(InputTokens), d_model, max_seq)
+    f_rec = RecurrentModule(d_model, 4, d_model)
+
+    print(f"input params: {sum(p.numel() for p in f_input.parameters()):,}")
+    print(f"rec params: {sum(p.numel() for p in f_rec.parameters()):,}")
 
     x, y = d_train[0]
     save_image("test-data/train.png", x)
@@ -25,5 +33,16 @@ if __name__ == "__main__":
     y.unsqueeze_(0)  # NOTE(Nic): add batch dim for testing.
 
     x_seq, y_seq = tokenizer(x, y)
+    x_prime = f_input(x_seq)
 
-    out = f_input(x_seq)
+    z_h, z_l = get_dummy_vars(d_model)
+
+    print("encoded_seq", x_prime.shape)
+    print("z_h", z_h.shape)
+    print("z_l", z_l.shape)
+
+    z_out = f_rec(z_l, z_h, x_prime)
+
+    print("z_out", z_out.shape)
+
+    # batch_size, seq_len, d_model = x_prime.shape
